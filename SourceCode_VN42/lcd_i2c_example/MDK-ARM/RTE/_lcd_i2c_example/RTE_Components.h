5feddb281ae7e78b47a02c3eea8f1b638444c857
hoctrocuahome <vht1112014@gmail.com>
2020-11-14 09:20:20 +0700

    first commit

---
+++ SourceCode_VN42/lcd_i2c_example/MDK-ARM/RTE/_lcd_i2c_example/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lcd_i2c_example' 
+ * Target:  'lcd_i2c_example' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
